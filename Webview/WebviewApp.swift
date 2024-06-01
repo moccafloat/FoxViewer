fa7664336020efa640fa93639c5ae4d1f99a547b
thecatworker <jasontheopilus123@gmail.com>
2024-06-01 15:14:14 +0700

    Initial Commit

---
+++ Webview/WebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebviewApp.swift
+//  Webview
+//
+//  Created by next on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
